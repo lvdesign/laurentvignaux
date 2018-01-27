690edf3c4c75acd18c5649fd364c2fd24949a07a
lvdesign <mailbox@lvdesign.com.fr>
2018-01-27 16:48:53 +0100

    sw5

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../laurentvignaux/service-worker.js').then(function(registration) {
+    navigator.serviceWorker.register('../laurentvignaux/service-worker.js', { scope: '/laurentvignaux/' }).then(function(registration) {
       // Registration was successful
+      console.log('Registration succeeded. Scope is ' + reg.scope);
     }).catch(function(err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
