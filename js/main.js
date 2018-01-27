7a8981ea64d8c44a96b852f86e097f58f02c3c96
lvdesign <mailbox@lvdesign.com.fr>
2018-01-27 16:51:37 +0100

    sw6

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../laurentvignaux/service-worker.js', { scope: '/laurentvignaux/' }).then(function(registration) {
+    navigator.serviceWorker.register('/laurentvignaux/service-worker.js', { scope: '/laurentvignaux/' }).then(function(registration) {
       // Registration was successful
       console.log('Registration succeeded. Scope is ' + reg.scope);
     }).catch(function(err) {
